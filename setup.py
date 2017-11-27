e644cf007fbfd9de88ffb3e846b7100dee8ce7f0
Piotr Held <pjheld@gmail.com>
2017-11-27 02:40:08 +0000

    setup.py: copy lights.so to working directory.

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 #!/bin/python
 
 from distutils.core import setup, Extension
+from shutil import copyfile
+
 
 lights_module = Extension('lights', sources = ['lights.c'])
 setup(name = 'Lights',
       version = '1.0',
       description = 'This is lights package',
       ext_modules = [lights_module])
+
+copyfile("build/lib.linux-armv6l-2.7/lights.so", "lights.so")
